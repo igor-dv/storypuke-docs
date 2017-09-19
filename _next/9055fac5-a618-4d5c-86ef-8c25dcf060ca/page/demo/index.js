914d49f187f4b8f94bd385b5c2fd283dddaf8d02
igor <davydkin.igor@gmail.com>
2017-09-19 10:26:52 +0300

    Update docs version to 3.2.9

---
+++ _next/9055fac5-a618-4d5c-86ef-8c25dcf060ca/page/demo/index.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/demo', function() {
+            var comp = module.exports=webpackJsonp([18],{411:function(e,t,l){e.exports=l(412)},412:function(e,t,l){"use strict";function a(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var n=l(0),u=a(n),d=l(9),r=a(d),c=l(18),f=a(c),o=l(19),i=a(o),m=r.default.div({display:"flex",alignItems:"center",justifyContent:"center",textAlign:"center",height:"calc(100vh - 50px)",width:"100vw"});t.default=(0,c.generator)("RootExample",function(e){var t=e.path;return u.default.createElement(f.default,null,u.default.createElement(i.default,{path:t}),u.default.createElement(m,null,u.default.createElement("div",null,u.default.createElement("h1",null,"TODO"),u.default.createElement("div",null,"I would like to add a real live demo here"))))})}},[411]);
+            return { page: comp.default }
+          })
+        
